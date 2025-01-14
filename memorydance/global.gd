619611889edd73a6469e08e68870bb821b643885
isabela-souzahara <111088925+isabela-souzahara@users.noreply.github.com>
2025-01-14 09:49:06 -0600

    Added a level test scene
    - Deleted repeat code for enemy model display
    - Added arrays for both player and enemy moves
    - Restricted player movement outside of player turn
    - Created a scene to test a normal level. Accounts for cases in which the player performs correctly and incorrectly and contains random generation of 4 enemy moves. Does not have a differentiation between the same move performed in a row.

--- memorydance/global.gd
+++ memorydance/global.gd
@@ -1,12 +1,15 @@
 extends Node
 
 # Player Variables:
+var player_turn = false
+var player_moves = []
 
 # idle = 0, up = 1, down = 2, right = 3, left = 4
 var player_position = 0
 
 
 # Enemy Variables:
+var enemy_moves = []
 
 # idle = 0, up = 1, down = 2, right = 3, left = 4
 var enemy_position = 0
@@ -15,43 +18,24 @@ var enemy_position = 0
 
 # Sets player's current position to "up"
 func player_movement_up():
-	player_position = 1
-	# Add movement to array
+	if player_turn == true:
+		player_position = 1
+		player_moves.append(1)
 
 # Sets player's current position to "down"
 func player_movement_down():
-	player_position = 2
-	# Add movement to array
+	if player_turn == true:
+		player_position = 2
+		player_moves.append(2)
 
 # Sets player's current position to "right"
 func player_movement_right():
-	player_position = 3
-	# Add movement to array
+	if player_turn == true:
+		player_position = 3
+		player_moves.append(3)
 
 # Sets player's current position to "left"
 func player_movement_left():
-	player_position = 4
-	# Add movement to array
-
-
-# Enemy Movements:
-
-# Sets enemy's current position to "up"
-func enemy_movement_up():
-	enemy_position = 1
-	# Add movement to array
-
-# Sets enemy's current position to "down"
-func enemy_movement_down():
-	enemy_position = 2
-	# Add movement to array
-
-# Sets enemy's current position to "right"
-func enemy_movement_right():
-	enemy_position = 3
-	# Add movement to array
-
-# Sets enemy's current position to "left"
-func enemy_movement_left():
-	enemy_position = 4
-	# Add movement to array
+	if player_turn == true:
+		player_position = 4
+		player_moves.append(4)
---
+++ memorydance/level_test.gd
@@ -0,0 +1,51 @@
+extends Node2D
+
+# Set the limit for enemy moves in pattern generation
+var move_limit = 4
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float):
+	# If the player has performed the same amount of moves as the enemy,
+	# end the player's turn
+	if Global.player_moves.size() == move_limit:
+		Global.player_turn = false
+		end_player_turn()
+
+# Handles results at the end of the player's turn 
+func end_player_turn():
+	# If the player matches the enemy pattern correctly, both arrays are
+	# cleared and a new enemy pattern is generated
+	if Global.player_moves == Global.enemy_moves:
+		print("correct")
+		Global.player_moves.clear()
+		Global.enemy_moves.clear()
+		generate_enemy_pattern(move_limit)
+	
+	# If the player does not match the enemy pattern correctly, player array
+	# is cleared while the enemy repeats the same pattern
+	else:
+		print("incorrect")
+		Global.player_moves.clear()
+		for i in Global.enemy_moves.size():
+			Global.enemy_position = Global.enemy_moves[i]
+			print(Global.enemy_moves)
+			$MoveTimer.start()
+			await $MoveTimer.timeout
+		
+		# Player turn begins
+		Global.player_turn = true
+
+# Generates enemy pattern in an array while enemy performs
+func generate_enemy_pattern(moves: int):
+	for i in moves:
+		Global.enemy_position = randi_range(1,4)
+		Global.enemy_moves.append(Global.enemy_position)
+		print(Global.enemy_moves)
+		$MoveTimer.start()
+		await $MoveTimer.timeout
+	
+	# Player turn begins
+	Global.player_turn = true
+
+func _on_timer_timeout():
+	generate_enemy_pattern(move_limit)
